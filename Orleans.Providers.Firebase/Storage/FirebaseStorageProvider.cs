924eb88c340519a4fd0ffbee0550906854777df7
Roger Creyke <creyke@gmail.com>
2017-02-18 22:46:44 +0000

    Added FirebaseStorageProvider.

---
+++ Orleans.Providers.Firebase/Storage/FirebaseStorageProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Orleans.Runtime;
+using Orleans.Storage;
+
+namespace Orleans.Providers.Firebase.Storage
+{
+    public class FirebaseStorageProvider : IStorageProvider
+    {
+        public Logger Log { get; set; }
+
+        public string Name { get; set; }
+
+        public Task ClearStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Init(string name, IProviderRuntime providerRuntime, IProviderConfiguration config)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ReadStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task WriteStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
